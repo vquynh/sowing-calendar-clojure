d260d0a36e3b26e813433d14b0dbf49f892883a7
Quynh Nguyen <quynh.nguyen@fielmann.com>
2022-02-06 20:38:13 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject sowingcalendar "0.1.0-SNAPSHOT"
+  :description "A sowingcalendar clone storing data in-memory"
+  :url "https://github.com/vquynh/sowingcalendar"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main sowingcalendar.main
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [aleph "0.4.6"]
+                 [hiccup "1.0.5"]
+                 [bidi "2.1.3"]])
